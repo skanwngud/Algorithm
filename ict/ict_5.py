58a496c2d8ff240d4716dcddc0c74ddbfc2ede8f
skanwngud <skanwngud@gmail.com>
2021-04-30 14:41:13 +0900

    commit

--- ict/ict_5.py
+++ ict/ict_5.py
@@ -6,3 +6,18 @@
 # 문자열을 특정한 기준에 따라서 끊어 처리해야하는 문제
 # 적절한 라이브러리를 찾아서 사용해야하는 문제
 
+for i in range(5):
+    for j in range(5):
+        print('(', i, ',', j,')', end = '')
+    print()
+
+
+dx = [0, -1, 0, 1]
+dy = [1, 0, -1, 0]
+
+x,y = [2, 2]
+
+for i in range(4):
+    nx = x + dx[i]
+    ny = y + dy[i]
+    print(nx, ny)
