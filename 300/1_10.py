65efca3e54eecc0187f55580ad51b78c3ff6ba26
skanwngud <skanwngud@gmail.com>
2022-03-25 21:13:35 +0900

    2022-03-25 300제 1~10

---
+++ 300/1_10.py
@@ -0,0 +1,35 @@
+# 1. Hello World 문자열을 출력하세요
+print("Hello World")
+
+# 2. 화면에 Mary's cosmetics 를 출력하세요. (중간에 '가 있음에 주의하세요.)
+print("mary's cosmetics")
+
+# 3. 신씨가 소리질렀다. "도둑이야". 를 출력하세요.
+print('신씨가 소리질렀다. "도둑이야".')
+
+# 4. "C:/Wndows" 를 출력하세요.
+print('"C:/Windows"')
+
+# 5. print("안녕하세요. \n만나서\t\t반갑습니다.")
+# \n: 줄바꿈
+# \t: indenct (4 spaces)
+
+# 6. print("오늘은", "일요일") 의 예측
+# 오늘은일요일
+print("오늘은", "일요일")
+
+# 7. print() 함수를 사용하여 다음과 같이 출력하세요.
+# naver;kakao;sk;samsung
+print('naver;kakao;sk;samsung')
+print('naver', 'kakao', 'sk', 'samsung', sep=';')
+
+# 8. print() 함수를 사용하여 다음과 같이 출력하세요.
+# naver/kakao/sk/samsung
+print('naver/kakao/sk/samsung')
+print('naver', 'kakao', 'sk', 'samsung', sep='/')
+
+# 9. 줄바꿈 없이 출력하세요. (힌트: end=) print 함수는 두 번 사용, 세미콜론은 한 줄에 여러개의 명령어를 작성하기 위해 사용.
+print("first", end='');print("sencond")
+
+# 10. 5/3 의 결과를 화면에 출력하세요.
+print(5/3)
