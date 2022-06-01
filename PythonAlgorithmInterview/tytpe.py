042d2e13460a4a5845c14fc283c0e176987ec216
skanwngud <skanwngud@gmail.com>
2022-06-01 13:40:07 +0900

    2022-06-01

--- PythonAlgorithmInterview/tytpe.py
+++ PythonAlgorithmInterview/tytpe.py
@@ -4,3 +4,21 @@ print(False == 1)
 print(False == 0)
 
 print(int(True))
+
+a = 10
+b = a
+print(f"a = 10 : {id(a)}")
+print(f"b = a: {id(b)}")
+print(f"int 10: {id(10)}")
+print(id(a) == id(b))
+print(id(a) is id(b))
+
+a = [1, 2, 3, 4]
+print(id(a))
+print(id([1, 2, 3, 4]))
+
+b = a
+a[1] = 0
+print(id(b))
+print(id(a))
+print(id([1, 0, 3, 4]))
