2332ee2832f4fa5aaba15f17833964d35def3520
skanwngud <skanwngud@gmail.com>
2022-06-07 23:23:54 +0900

    2022-06-07

--- PythonAlgorithmInterview/3_7_2_trapping_rain.py
+++ PythonAlgorithmInterview/3_7_2_trapping_rain.py
@@ -2,6 +2,15 @@
 # 높이를 입력 받아 비 온 후 얼마나 많은 물이 쌓일 수 있는지 계산하라
 
 # 투 포인터
+def solution(height):
+    max_height = max(height)
+    width = len(height)
+    total_space = max_height * width
+    print(total_space)
+    
+    height_idx = height.index(max(height))
+    print(height_idx)
+
 def trap(height):
     if not height:
         return 0
@@ -27,3 +36,4 @@ if __name__ == "__main__":
     rain = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
     print(trap(rain))
     
+    solution(rain)
