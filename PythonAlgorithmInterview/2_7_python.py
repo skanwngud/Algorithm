e97d15d45fa17c52a7d3c8a500b9cd2b6f8b9561
skanwngud <skanwngud@gmail.com>
2022-08-06 21:53:06 +0900

    2022-08-06

---
+++ PythonAlgorithmInterview/2_7_python.py
@@ -0,0 +1,7 @@
+a = {}
+print(type(a))
+
+a = {1, 2, 3}
+print(type(a))
+
+a.add(1)
---
+++ PythonAlgorithmInterview/5_0_2_dict.py
@@ -0,0 +1,8 @@
+a = {"a": 0}
+
+"""
+len(a) == O(1)
+a[key] == O(1)
+a[key] = value == O(1)
+key in a == O(1)
+"""
