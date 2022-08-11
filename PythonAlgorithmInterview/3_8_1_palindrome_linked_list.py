c68d6356c5ac2865f40e8823c079aa54d9394569
skanwngud <skanwngud@gmail.com>
2022-08-11 23:51:46 +0900

    2022-08-11

--- PythonAlgorithmInterview/3_8_1_palindrome_linked_list.py
+++ PythonAlgorithmInterview/3_8_1_palindrome_linked_list.py
@@ -1,8 +1,17 @@
 # 연결 리스트가 팰린드롬 구조인지 판별하라
+import collections
+
 
 class Solution:
     def solution(self, nums):
-        nums = list(nums)
+        q = []
+
+        node = nums
+        while node is not None:
+            q.append(node.val)
+            node = node.next
+
+
         for idx in range(len(nums)):
             left = idx
             right = len(nums) - idx
@@ -13,10 +22,18 @@ class Solution:
         return True
 
     def solution2(self, head):
-        q = []
+        q = collections.deque()
+
+        if head is None:
+            return True
 
         node = head
         while node is not None:
             q.append(node.val)
             node = node.next
 
+        while len(q) > 1:
+            if q.popleft() != q.pop():
+               return False
+
+        return True
